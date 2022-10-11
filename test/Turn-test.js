44fe1d1ed1812062b5036260a54f324e972b3b40
Brett Kuhn <brettkuhn87@gmail.com>
2022-10-10 20:41:34 -0600

    Wrote tests to test whether Turn takes two arguments per spec

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
 
@@ -10,5 +11,15 @@ describe('Turn', function() {
     expect(Turn).to.be.a('function');
   });
 
+  it('should take a guess', function() {
+    const turn = new Turn('object');
+    expect(turn.guess).to.equal('object');
+  });
 
+  it('should take a Card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+
+    expect(turn.card).to.deep.equal(card)
+  });
 });
