9aac7dbb3ef9460da8c684bb2c5c72bbed44ff71
Brett Kuhn <brettkuhn87@gmail.com>
2022-10-13 08:38:08 -0600

    Added tests and code to test for current round property and having a start function

--- src/Game.js
+++ src/Game.js
@@ -8,8 +8,16 @@ const Turn = require('../src/Turn');
 
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = {};
 
+  }
+  start() {
+    const cardDeck = data.prototypeData.map()
+      // want to take the data array and loop through
+      // each time, want to instantiate a new Card and have it go 
+      //    to an arry, so a iterator that creates a new array
+  }
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
@@ -20,4 +28,14 @@ class Game {
   }
 }
 
+
+// Needs "keep track of currentRound" --> currentRound property
+//  - Round object that has been instantiated
+//  - Where instantiated? Outside of this?
+
+// start method
+// - creates cards
+// - puts cards in deck
+// - creates a new Round using the deck
+// - invokes printMessage and printQuestion
 module.exports = Game;
--- test/Game-test.js
+++ test/Game-test.js
@@ -5,7 +5,27 @@ const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 const Round = require('../src/Round');
 const Turn = require('../src/Turn');
+const Game = require('../src/Game');
+const data = require('../src/data');
+
 
 describe('Round', function() {
+  it('should have a place to keep track of the current round', function() {
+    const game = new Game();
+
+    expect(game).to.have.a.key('currentRound');
+  });
+  //need another test to make sure it actually IS the current round (check other tests for similar issues)
+
+  it('should be able to start everything', function() {
+    const game = new Game();
+
+    expect(game.start).to.be.a('function');
+  });
+
+  it('should create cards', function() {
+    // console.log('array of data: ', data.prototypeData);
+
+  });
 
 });
